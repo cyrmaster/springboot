f234eb0c50abe879a5875f7dcd3c29e47a272955
chenyangrong <chenyangrong@fmcc.boss>
2020-04-03 15:48:24 +0800

    v2.修改ajax

--- src/main/java/com/cyr/springboot/Controller/TestController.java
+++ src/main/java/com/cyr/springboot/Controller/TestController.java
@@ -1,15 +1,21 @@
 package com.cyr.springboot.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/Test")
+@Controller
+@RequestMapping("/Html")
 public class TestController {
 
     @RequestMapping("/login")
     public String login()
     {
         return  "login";
+    }
+    @RequestMapping("ajaxtest")
+    public String ajaxtest()
+    {
+        return "ajaxtest";
     }
 }
