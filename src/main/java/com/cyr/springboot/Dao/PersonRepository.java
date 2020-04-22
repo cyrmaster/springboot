f43337a493c58a171592a2cb716933782a522e5f
chenyangrong <chenyangrong@fmcc.boss>
2020-04-22 17:27:10 +0800

    v5.自动配置实例

---
+++ src/main/java/com/cyr/springboot/Dao/PersonRepository.java
@@ -0,0 +1,22 @@
+package com.cyr.springboot.Dao;
+
+import com.cyr.springboot.bean.Person;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface PersonRepository extends JpaRepository<Person,Long> {
+
+    List<Person> findByName(String name);
+
+    Person findByAddressAndName(String address,String name);
+
+    @Query("select  p from Person  p where  p.name=:name and p.address=:address")
+    Person winthNameAndAddressQuery(@Param ("name")String name,@Param ("address")String address));
+
+    List<Person> withNameAndAddressNamedQuery(String name,String address);
+
+}
